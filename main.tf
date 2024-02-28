59b70ac2059e8308ebf4e090921c07eb72d3bb88
viveksachdev <viveksachdev8@gmail.com>
2024-02-28 10:13:44 +0530

    commented credentials

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                   = "us-east-1"
-  profile                  = "vivek"
-  shared_config_files      = ["/home/vivek/.aws/config"]
-  shared_credentials_files = ["/home/vivek/.aws/credentials"]
+  # profile                  = "vivek"
+  # shared_config_files      = ["/home/vivek/.aws/config"]
+  # shared_credentials_files = ["/home/vivek/.aws/credentials"]
 }
