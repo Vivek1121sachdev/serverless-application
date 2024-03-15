9006f1136f84b318f4589ceb910dd123baa14d62
viveksachdev <viveksachdev8@gmail.com>
2024-03-15 18:24:02 +0530

    ecr: data block repo name corrected

--- ecr.tf
+++ ecr.tf
@@ -32,7 +32,7 @@ EOF
 }
 
 data "aws_ecr_image" "image" {
-  repository_name = "${aws_ecr_repository.repository_name}"
+  repository_name = "${aws_ecr_repository.repository_name.name}"
   most_recent       = true
 }
 
