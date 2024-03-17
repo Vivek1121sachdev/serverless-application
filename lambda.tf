319f245f0d6a10d226304c53ef0dd452825e9c83
viveksachdev <viveksachdev8@gmail.com>
2024-03-17 08:13:34 +0530

    lambda: image uri change

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "lambda-function" {
   function_name = var.function_name
   timeout       = var.lambda_timeout
-  image_uri     = "${aws_ecr_repository.repository_name.repository_url}:${data.aws_ecr_image.image.image_tag}"
+  image_uri     = "${aws_ecr_repository.repository_name.repository_url}:${data.aws_ecr_image.image.id}"
   package_type  = "Image"
 
   role = aws_iam_role.lambda-role.arn
