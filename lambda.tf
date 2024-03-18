362bd19057594b0a0886423e5ce8023da5e556db
viveksachdev <viveksachdev8@gmail.com>
2024-03-18 11:13:42 +0530

    lambda: image uri changes

--- lambda.tf
+++ lambda.tf
@@ -1,18 +1,14 @@
 resource "aws_lambda_function" "lambda-function" {
   function_name = var.function_name
   timeout       = var.lambda_timeout
-  image_uri     = "${aws_ecr_repository.repository_name.repository_url}:${data.local_file.image-tag.content}"
+  image_uri     = "${aws_ecr_repository.repository_name.repository_url}:${data.aws_ecr_image.image.most_recent}"
   package_type  = "Image"
 
   role = aws_iam_role.lambda-role.arn
   # depends_on = [ null_resource.build-img-script ]
 }
 
-# data "aws_ecr_image" "image" {
-#   repository_name = aws_ecr_repository.repository_name.name
-#   most_recent     = true
-# }
-
-data "local_file" "image-tag" {
-  filename = "gitHashValue.txt"
+data "aws_ecr_image" "image" {
+  repository_name = aws_ecr_repository.repository_name.name
+  most_recent     = true
 }
--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "api-gw-execution-arn" {
 output "ecr_uri"{
   value = aws_ecr_repository.repository_name.repository_url
 }
+
+output "image_uri" {
+  value = data.aws_ecr_image.image.image_uri
+}
+
+output "image_tag" {
+  value = data.aws_ecr_image.image.image_tags[0]
+}
