3ccc1384d384b2ed9b9d2f8b5ccb84cc55f7f45d
viveksachdev <viveksachdev8@gmail.com>
2024-04-05 12:35:48 +0530

    api-gw: removed method and headers from response var

--- Backend/modules/api-gw/variable.tf
+++ Backend/modules/api-gw/variable.tf
@@ -28,8 +28,6 @@ variable "stage_name" {
 variable "integration_response_parameters" {
   type = map
   default = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
     "method.response.header.Access-Control-Allow-Origin"  = "'http://serverless-application-frontend-code.s3-website.us-east-1.amazonaws.com'"
   }
 }
