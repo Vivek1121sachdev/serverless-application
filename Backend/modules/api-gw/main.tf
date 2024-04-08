c83c8852facf82271b07b2633c732dfc33b9e747
viveksachdev <viveksachdev8@gmail.com>
2024-04-08 17:07:39 +0530

    api-gw:student method change to ANY

--- Backend/modules/api-gw/main.tf
+++ Backend/modules/api-gw/main.tf
@@ -33,20 +33,22 @@ resource "aws_api_gateway_method_response" "method_response_health" {
 }
 
 resource "aws_api_gateway_method" "student-methods" {
-  for_each = local.methods
+  # for_each = local.methods
 
   rest_api_id   = aws_api_gateway_rest_api.serverless-app.id
   resource_id   = aws_api_gateway_resource.resources["student"].id
-  http_method   = each.value
+  http_method   = "ANY"
+  # http_method   = each.value
   authorization = var.authorization
 }
 
 resource "aws_api_gateway_method_response" "method_response_student" {
-  for_each = local.methods
+  # for_each = local.methods
 
   rest_api_id = aws_api_gateway_rest_api.serverless-app.id
   resource_id = aws_api_gateway_resource.resources["student"].id
-  http_method = each.value
+  # http_method = each.value
+  http_method = aws_api_gateway_method.student-methods.http_method
   status_code = "200"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin" = true
@@ -105,22 +107,24 @@ response_parameters = {
 }
 
 resource "aws_api_gateway_integration" "student-integration" {
-  for_each = aws_api_gateway_method.student-methods
+  # for_each = aws_api_gateway_method.student-methods
 
   rest_api_id             = aws_api_gateway_rest_api.serverless-app.id
   resource_id             = aws_api_gateway_resource.resources["student"].id
-  http_method             = each.value.http_method
+  # http_method             = each.value.http_method
+  http_method             = aws_api_gateway_method.student-methods.http_method
   integration_http_method = var.integration_http_method
   type                    = var.integration_type
   uri                     = var.lambda_invoke_arn
 }
 
 resource "aws_api_gateway_integration_response" "integration_response_student" {
-  for_each = aws_api_gateway_method.student-methods
+  # for_each = aws_api_gateway_method.student-methods
 
   rest_api_id = aws_api_gateway_rest_api.serverless-app.id
   resource_id = aws_api_gateway_resource.resources["student"].id
-  http_method = each.value.http_method
+  # http_method = each.value.http_method
+  http_method = "ANY"
   status_code = "200"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
