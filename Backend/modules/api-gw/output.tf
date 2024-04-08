4dbe04e54c6092564c282d816c1d51d5d248e3f3
viveksachdev <viveksachdev8@gmail.com>
2024-04-08 18:01:28 +0530

    test:api gateway invoke url storing in file

--- Backend/modules/api-gw/output.tf
+++ Backend/modules/api-gw/output.tf
@@ -1,3 +1,7 @@
 output "execution_arn" {
   value = aws_api_gateway_rest_api.serverless-app.execution_arn
 }
+
+output "invoke_url" {
+  value = aws_api_gateway_rest_api.serverless-app.invoke_url
+}
