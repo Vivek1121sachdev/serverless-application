2f39bf80d3225e241dd6fee6f787c9e08fc8a5e5
viveksachdev <viveksachdev8@gmail.com>
2024-04-08 18:05:47 +0530

    api gateway invoke url source changed

--- Backend/modules/api-gw/output.tf
+++ Backend/modules/api-gw/output.tf
@@ -3,5 +3,5 @@ output "execution_arn" {
 }
 
 output "invoke_url" {
-  value = aws_api_gateway_rest_api.serverless-app.invoke_url
+  value = aws_api_gateway_deployment.api-gw-deployment.invoke_url
 }
