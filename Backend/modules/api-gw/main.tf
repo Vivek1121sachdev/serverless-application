963de5c3fd2fc37377e347e1fd09c14baf199dda
viveksachdev <viveksachdev8@gmail.com>
2024-04-11 11:35:30 +0530

    api-gw:comments added

--- Backend/modules/api-gw/main.tf
+++ Backend/modules/api-gw/main.tf
@@ -28,7 +28,7 @@ resource "aws_api_gateway_method_response" "method_response_health" {
   http_method = aws_api_gateway_method.health_get_method.http_method
   status_code = 200
   response_parameters = {
-~    "method.response.header.Access-Control-Allow-Origin"  = true
+   "method.response.header.Access-Control-Allow-Origin"  = true
     "method.response.header.Access-Control-Allow-Methods" = true
     "method.response.header.Access-Control-Allow-Headers" = true
   }
