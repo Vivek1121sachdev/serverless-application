ddee668d5f6ebc46c45c6f6fb83a1c8a5bad24a1
viveksachdev <viveksachdev8@gmail.com>
2024-04-12 11:07:02 +0530

    cloudwatch log-group attached to lambda

--- Backend/modules/lambda/main.tf
+++ Backend/modules/lambda/main.tf
@@ -11,6 +11,8 @@ resource "aws_lambda_function" "lambda-function" {
 
   logging_config {
     log_format = "Text"
+    log_group = aws_cloudwatch_log_group.lambda-log-group.name
+    
   }
 
   depends_on = [
