f5aac44e170d4c744f2ea5db22156184874be5f9
viveksachdev <viveksachdev8@gmail.com>
2024-04-18 17:10:57 +0530

    added static and dynamic values to lambda env-var

--- Backend/data.tf
+++ Backend/data.tf
@@ -1,17 +1,23 @@
+// Getting the most recent ECR image from the repository //
 data "aws_ecr_image" "image" {
   repository_name = module.ecr.repository_name
   most_recent     = true
 }
 
 locals {
+  // CloudWatch Alarm names //
   cloudWatch_Alarm = ["4XXError", "5XXError"]
+
+  // API-GW resource names and methods //
   resources = {
-    health = "GET",
-    student = "ANY",
+    health   = "GET",
+    student  = "ANY",
     students = "GET"
   }
+
+  // SSM parameter keys and values //
   parameters = {
-    "/serverless-application/dynamodb/dbTableName"   = "students-data"
-    "mySecondParam" = "foo"
+    "/serverless/dynamodb/dbTableName" = "students-data",
+    "mySecondParam"                    = "foo"
   }
 }
